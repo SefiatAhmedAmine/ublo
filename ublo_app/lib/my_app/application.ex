18da473c605800b57db4326de9be91993290e1ce
SefiatAhmedAmine <amine-sefiat@hotmail.com>
2026-04-16 22:30:01 +0100

    create ecto Repo
    check connexion with MyApp.Repo.query!("SELECT 1")

--- ublo_app/lib/my_app/application.ex
+++ ublo_app/lib/my_app/application.ex
@@ -8,8 +8,7 @@ defmodule MyApp.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: MyApp.Worker.start_link(arg)
-      # {MyApp.Worker, arg}
+      MyApp.Repo
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
